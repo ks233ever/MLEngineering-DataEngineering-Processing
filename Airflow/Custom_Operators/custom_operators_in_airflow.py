392d430e62348b2be9631cea0ce25b1f970f02ab
Kayla Every <42351537+ks233ever@users.noreply.github.com>
2020-07-14 00:00:22 -0400

    Add files via upload

---
+++ Airflow/Custom_Operators/custom_operators_in_airflow.py
@@ -0,0 +1,90 @@
+# Consolidate repeated code into custom Operator Plugins, can easily be reused going forward
+## HasRowsOperator, a custom data quality check to ensure rows were inserted
+## S3toRedshift, custom operator
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+
+from airflow.operators import (
+    HasRowsOperator,
+    PostgresOperator,
+    PythonOperator,
+    S3ToRedshiftOperator
+)
+
+import sql_statements
+
+
+dag = DAG(
+    "lesson3.exercise1",
+    # because start date in past, will backfill
+    start_date=datetime.datetime(2018, 1, 1, 0, 0, 0, 0),
+    end_date=datetime.datetime(2018, 12, 1, 0, 0, 0, 0),
+    schedule_interval="@monthly",
+    # So DAGs cannot run in parallel
+    max_active_runs=1
+)
+
+create_trips_table = PostgresOperator(
+    task_id="create_trips_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+
+copy_trips_task = S3ToRedshiftOperator(
+    task_id="load_trips_from_s3_to_redshift",
+    dag=dag,
+    table="trips",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udac-data-pipelines",
+    s3_key="divvy/partitioned/{execution_date.year}/{execution_date.month}/divvy_trips.csv"
+)
+
+
+check_trips = HasRowsOperator(
+    task_id='check_trips_data',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table = 'trips',
+    provide_context=True
+)
+
+create_stations_table = PostgresOperator(
+    task_id="create_stations_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_STATIONS_TABLE_SQL,
+)
+
+copy_stations_task = S3ToRedshiftOperator(
+    task_id="load_stations_from_s3_to_redshift",
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udac-data-pipelines",
+    s3_key="divvy/unpartitioned/divvy_stations_2017.csv",
+    table="stations"
+)
+
+#
+# TODO: Replace this data quality check with the HasRowsOperator
+#
+check_stations = HasRowsOperator(
+    task_id='check_stations_data',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table = 'stations',
+    provide_context=True
+)
+
+create_trips_table >> copy_trips_task
+create_stations_table >> copy_stations_task
+copy_stations_task >> check_stations
+copy_trips_task >> check_trips
